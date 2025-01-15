d8568251d13d1f67147e763227d0de9d7ce78b65
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2025-01-15 11:42:34 +0100

    Updated config files for this to work on Azure

--- Task1/numint1.py
+++ Task1/numint1.py
@@ -34,4 +34,6 @@ def integrate(lower: float, upper: float) -> Response:
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    import os
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
